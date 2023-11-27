52252a1105f72a80a7358a51c9c8131848990134
A-Damien <125483784+A-Damien@users.noreply.github.com>
2023-11-26 23:26:36 -0500

    Delete yelpiversity directory

--- yelpiversity/client/src/components/partials/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export const Footer = (props) => {
-    return (
-        <footer className="footer bg-dark py-3 mt-auto">
-            <div className="container">
-                <span className="text-light">&copy;Knightro's List 2023</span>
-            </div>
-        </footer>
-    );
-};
