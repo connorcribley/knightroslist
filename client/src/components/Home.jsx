f6533807d30b71c772e65f52577aebac33b0be16
Connor Cribley <55695937+connorcribley@users.noreply.github.com>
2024-01-31 20:01:49 -0500

    Edited Home styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import { Navbar } from "./partials/Navbar";
 import { Footer } from "./partials/Footer";
+import "./Home.css";
 import Background from "./ucf1.png"
 
 const Home = (props) => {
@@ -7,8 +8,19 @@ const Home = (props) => {
 
     return (
         <html lang="en">
-        
-            <div id="intro">
+            <Navbar />
+            <body className="d-flex text-center text-white bg-dark home-body">
+                <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column justify-content-center">
+                    <main className="px-3 main-home m rounded">
+                        <h1 className="mt-2">Knightro's List</h1>
+                        <p className="lead">Explore popular locations at the University of Central Florida!</p>
+                        <a href="/universities" className="btn btn-lg btn-secondary font-weight-bold text-dark bg-light mb-3">View Locations</a>
+                    </main>
+                </div>
+            </body>
+            <Footer />
+
+            {/* <div id="intro">
             <div id="overlay" style={{background: 'rgba(0, 0, 0, 0.2)'}}>
                 <body style={{display: 'table', padding: 'table', backgroundImage: "url(" + Background + ")", backgroundPosition:"center", backgroundRepeat:"no-repeat", backgroundColor:"rgb(255,215,0)"}} className="d-flex flex-column vh-100">
                     <Navbar />
@@ -17,7 +29,7 @@ const Home = (props) => {
                     
                         <main className="text-center align-self-end" style={{background: "content-box black", outlineStyle:"groove", outlineColor:"black", textAlign:"center",blockSize:"105%"}}>
                             <h1 style={{border:"solid", borderBlockColor:"black",background:"yellow", justifyContent:"center"}}>Welcome to Knightro's List!</h1>
-                            <a href="/universities" style={{}} className="btn btn-lg btn-secondary text-secondary fw-bold bg-white">Check Out Popular Locations</a>
+                            <a href="/universities" style={{}} className="btn btn-lg btn-secondary text-secondary fw-bold text-white bg-white">Check Out Popular Locations</a>
                         </main>
 
                     </main>
@@ -25,7 +37,7 @@ const Home = (props) => {
                     <Footer />
                 </body>
                 </div>
-            </div>
+            </div> */}
         </html>
     );
 };
