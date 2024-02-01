d8a2ace7d49f7b296b98d31548ddf69c9c625708
Connor Cribley <55695937+connorcribley@users.noreply.github.com>
2024-01-31 20:03:10 -0500

    Deleted comments

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -19,25 +19,6 @@ const Home = (props) => {
                 </div>
             </body>
             <Footer />
-
-            {/* <div id="intro">
-            <div id="overlay" style={{background: 'rgba(0, 0, 0, 0.2)'}}>
-                <body style={{display: 'table', padding: 'table', backgroundImage: "url(" + Background + ")", backgroundPosition:"center", backgroundRepeat:"no-repeat", backgroundColor:"rgb(255,215,0)"}} className="d-flex flex-column vh-100">
-                    <Navbar />
-                    
-                    <main className="container mt-5">
-                    
-                        <main className="text-center align-self-end" style={{background: "content-box black", outlineStyle:"groove", outlineColor:"black", textAlign:"center",blockSize:"105%"}}>
-                            <h1 style={{border:"solid", borderBlockColor:"black",background:"yellow", justifyContent:"center"}}>Welcome to Knightro's List!</h1>
-                            <a href="/universities" style={{}} className="btn btn-lg btn-secondary text-secondary fw-bold text-white bg-white">Check Out Popular Locations</a>
-                        </main>
-
-                    </main>
-                    
-                    <Footer />
-                </body>
-                </div>
-            </div> */}
         </html>
     );
 };
